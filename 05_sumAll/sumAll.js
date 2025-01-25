b48b418040ab54681585249216469e2b1690d42d
d1scipline <143661704+d1scipline@users.noreply.github.com>
2025-01-25 21:17:00 +0300

    add the solution code for sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, secondNum) {
+    let sum = 0;
+    if (firstNum < 0 || secondNum < 0 || !Number.isInteger(firstNum) || !Number.isInteger(secondNum)) {
+        return "ERROR";
+    }
+    if (firstNum > secondNum) {
+        for (let i = secondNum; i <= firstNum; i++) {
+            sum += i;
+        }
+    }
+    else {
+        for (let i = firstNum; i <= secondNum; i++) {
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
