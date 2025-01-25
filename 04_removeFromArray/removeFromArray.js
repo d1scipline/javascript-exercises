699f621a175066f870037ee2b551b2a506f4df75
d1scipline <143661704+d1scipline@users.noreply.github.com>
2025-01-25 20:58:43 +0300

    add the solution for the removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
 const removeFromArray = function() {
+    let valueCount = arguments.length - 1;
+    let inputArray = arguments[0];
+    for (let i = 1; i <= valueCount; i++) {
+        while (inputArray.indexOf(arguments[i]) != -1) {
+            inputArray.splice(inputArray.indexOf(arguments[i]), 1);
+        }
+    }
+    return inputArray;
 };
 
 // Do not edit below this line
