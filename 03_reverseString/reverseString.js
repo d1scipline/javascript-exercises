f85f8ced4a2bc0f25367a946a479b468bcb8e7bd
d1scipline <143661704+d1scipline@users.noreply.github.com>
2025-01-25 20:28:26 +0300

    add the code for reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    let stringLength = inputString.length;
+    let result = "";
+    if (stringLength > 0) {
+        for (let i = stringLength - 1; i >= 0; i--) {
+            result += inputString[i];
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
