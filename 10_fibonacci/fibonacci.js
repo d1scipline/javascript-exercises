8cbc83a187f7c42cca932e1d4389ec1ce40eb4ed
d1scipline <143661704+d1scipline@users.noreply.github.com>
2025-02-04 22:33:48 +0300

    add solution for the 10th challenge

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {   
+    if (number < 0) {
+        return "OOPS";
+    }
+    let a = 0;
+    let b = 1;
+    let c = a+b;
+    for (let i = 0; i < number; i++) {
+        a = b;
+        b = c;
+        c = a+b;
+    }
+    return a;
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
