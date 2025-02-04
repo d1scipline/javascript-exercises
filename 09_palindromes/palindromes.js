7a9584acfc95f80a0077c7dd58e3fc34374c5c2d
d1scipline <143661704+d1scipline@users.noreply.github.com>
2025-02-04 22:15:17 +0300

    add the solution for the 9th challenge

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,26 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    //remove spaces
+    word = word.replaceAll(" ", "");
 
-};
+    //turn the word into an array, filter out punctuation and make it all lowercase
+    let word_filtered = word.split("")
+    .filter((item) => {
+        if (item == "," || item == "." || item == "!" || item == "?" || item == ";" || item == ":") {
+            return false;
+        }
+        else {
+            return true;
+        }
+    })
+    .map((item) => item.toLowerCase());
+    
+    //reverse the word (using .slice() to create a copy and not reverse the original array)
+    let word_reversed = word_filtered.slice().reverse().join("");
+    word_filtered = word_filtered.join("");
+
+    return word_filtered === word_reversed;
+}
+
 
 // Do not edit below this line
 module.exports = palindromes;
