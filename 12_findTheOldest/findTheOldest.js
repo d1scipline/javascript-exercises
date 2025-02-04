7745ac28cc55e607014e02260bda3473c682abfd
d1scipline <143661704+d1scipline@users.noreply.github.com>
2025-02-04 22:56:32 +0300

    add solution for the 12th exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        if (((oldest.yearOfDeath || new Date().getFullYear()) - oldest.yearOfBirth) > ((person.yearOfDeath || new Date().getFullYear()) - person.yearOfBirth)) {
+            return oldest;
+        } 
+        else {
+            return person;
+        }
+    }, people[0]);
 };
 
 // Do not edit below this line
